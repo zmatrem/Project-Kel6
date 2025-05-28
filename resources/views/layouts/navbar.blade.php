6b3a6ee44874435f5f8c3862d42f84971392461f
Fema19 <fachrinurulakbar19@gmail.com>
2025-05-28 08:52:30 +0700

    first commit

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,68 @@
+ <div class="navbar-offcanvas navbar-offcanvas-touch navbar-offcanvas-right" id="js-bootstrap-offcanvas">
+                                <button type="button" class="offcanvas-toggle closecanvas" data-toggle="offcanvas" data-target="#js-bootstrap-offcanvas">
+                                   <i class="fa fa-times fa-2x" aria-hidden="true"></i>
+                                </button>
+
+                                    <ul id="menu-main-menu" class="nav navbar-nav navbar-right navbar-primary sf-menu"><li id="menu-item-2810" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-2810"><a href="https://myticket.kenzap.com/upcoming-events/">Upcoming</a>
+<ul class="sub-menu">
+	<li id="menu-item-3732" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3732"><a href="https://myticket.kenzap.com/events-search-sport/">Sport Events</a></li>
+	<li id="menu-item-2941" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2941"><a href="https://myticket.kenzap.com/past-events/">Past Events</a></li>
+	<li id="menu-item-3251" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3251"><a href="https://myticket.kenzap.com/event-grid/">Event Grid</a></li>
+	<li id="menu-item-2972" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2972"><a href="https://myticket.kenzap.com/home-v3/">BWM Golf</a></li>
+	<li id="menu-item-2973" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2973"><a href="https://myticket.kenzap.com/home-v2/">Performance</a></li>
+	<li id="menu-item-2805" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2805"><a href="https://myticket.kenzap.com/about-us/">About</a></li>
+	<li id="menu-item-2942" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2942"><a href="https://myticket.kenzap.com/blog/">News</a></li>
+</ul>
+</li>
+<li id="menu-item-3547" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3547"><a href="https://myticket.kenzap.com/events/">Events</a></li>
+<li id="menu-item-3428" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-3428"><a href="https://myticket.kenzap.com/search-events/">Search</a>
+<ul class="sub-menu">
+	<li id="menu-item-2811" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2811"><a href="https://myticket.kenzap.com/select-ticket-without-seats-maroon-5/">Marron 5</a></li>
+	<li id="menu-item-2812" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2812"><a href="https://myticket.kenzap.com/single-event-beach-volley/">Beach Volley</a></li>
+	<li id="menu-item-2813" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2813"><a href="https://myticket.kenzap.com/single-event-guitar-vibes/">Guitar Vibes</a></li>
+	<li id="menu-item-2814" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2814"><a href="https://myticket.kenzap.com/artist-page/">Artist Page</a></li>
+	<li id="menu-item-3025" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3025"><a href="https://myticket.kenzap.com/home-v3/">Home v3</a></li>
+	<li id="menu-item-3026" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3026"><a href="https://myticket.kenzap.com/home-v2/">Home v2</a></li>
+	<li id="menu-item-3027" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3027"><a href="https://myticket.kenzap.com/checkout/">Checkout</a></li>
+</ul>
+</li>
+<li id="menu-item-2809" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2809"><a href="https://myticket.kenzap.com/image-gallery/">Gallery</a></li>
+<li id="menu-item-2806" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2806"><a href="https://myticket.kenzap.com/contact-us/">Contact</a></li>
+                                        <li class="cart"><a class="cart-count" href="https://myticket.kenzap.com/cart/">0</a></li>
+
+                                                                        </ul>
+                                    <ul id="menu-main-menu-1" class="nav navbar-nav navbar-right navbar-primary_mobile"><li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-2810"><a href="https://myticket.kenzap.com/upcoming-events/">Upcoming</a>
+<ul class="sub-menu">
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3732"><a href="https://myticket.kenzap.com/events-search-sport/">Sport Events</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2941"><a href="https://myticket.kenzap.com/past-events/">Past Events</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3251"><a href="https://myticket.kenzap.com/event-grid/">Event Grid</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2972"><a href="https://myticket.kenzap.com/home-v3/">BWM Golf</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2973"><a href="https://myticket.kenzap.com/home-v2/">Performance</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2805"><a href="https://myticket.kenzap.com/about-us/">About</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2942"><a href="https://myticket.kenzap.com/blog/">News</a></li>
+</ul>
+</li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3547"><a href="https://myticket.kenzap.com/events/">Events</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-3428"><a href="https://myticket.kenzap.com/search-events/">Search</a>
+<ul class="sub-menu">
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2811"><a href="https://myticket.kenzap.com/select-ticket-without-seats-maroon-5/">Marron 5</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2812"><a href="https://myticket.kenzap.com/single-event-beach-volley/">Beach Volley</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2813"><a href="https://myticket.kenzap.com/single-event-guitar-vibes/">Guitar Vibes</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2814"><a href="https://myticket.kenzap.com/artist-page/">Artist Page</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3025"><a href="https://myticket.kenzap.com/home-v3/">Home v3</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3026"><a href="https://myticket.kenzap.com/home-v2/">Home v2</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3027"><a href="https://myticket.kenzap.com/checkout/">Checkout</a></li>
+</ul>
+</li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2809"><a href="https://myticket.kenzap.com/image-gallery/">Gallery</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2806"><a href="https://myticket.kenzap.com/contact-us/">Contact</a></li>
+                                        <li class="cart"><a class="cart-count" href="https://myticket.kenzap.com/cart/">0</a></li>
+
+                                                                        </ul>
+
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
